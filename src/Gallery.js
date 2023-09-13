1a8904f3a0dd04e351834a9825a7e150f6e02499
DrewStroede <dstrow13@gmail.com>
2023-09-13 18:40:22 -0500

    got cards in rows

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
 import data from './data.json';
+import { Container, Row } from "react-bootstrap";
+
 // import { getByTitle } from '@testing-library/react';
 
 
@@ -19,7 +21,11 @@ class Gallery extends React.Component {
 
         return (
         <main>
-          {beasts}
+          <Container id='cards'>
+            <Row class='p-3' xs={1} sm={2} md= {3} lg={5} xl={6} xxl={7} >
+                {beasts}
+            </Row>
+          </Container>
         </main>
         )
       }
