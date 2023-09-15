1450a4b795ce17752ecf7c8ab31c6d85da3be1f4
DrewStroede <dstrow13@gmail.com>
2023-09-15 18:08:05 -0500

    reconfigured website to try and add map and modal

---
+++ src/SelectedBeast.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Modal} from "react-bootstrap";
+
+
+class SelectedBeast extends React.Component {
+
+    render() {
+        return (
+            <Modal show={this.props.showModal} onHide={this.props.hideBeastModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{this.props.beast.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <img src={this.props.beast.img_url} alt={this.props.beast.keyword}/>
+                </Modal.Body>
+                <Modal.Footer>
+        {/* //   <Button variant="primary" onClick={this.handleFavorites}>
+        //   <span role="img" aria-label="heart">💘Favorite</span> {this.state.favorites}
+        //   </Button> */} 
+        </Modal.Footer>
+            </Modal>
+        )
+    }
+}
+
+export default SelectedBeast;
